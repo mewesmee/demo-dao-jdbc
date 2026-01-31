cde4b8853e264422b7b8363fc782fd04105eb022
mewesmee <wesmehav@gmail.com>
2026-01-30 23:25:11 -0300

    FindById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
-	
-	public static createSellerDao() {
-		return new SellerDaoJDBC();
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
